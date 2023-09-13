8419615fbdf78dfd824e011ea2af4e6e29c9c39b
Richart Escobedo <rescobedoq@unsa.edu.pe>
2023-09-13 15:59:00 -0500

    fp2 notas en un archivo

---
+++ fase01/prac00/Notas2.java
@@ -0,0 +1,11 @@
+class Notas2{
+  public static void main(String args[]){
+   double[] percents = new double[6];
+   percents[0] = 0.05;
+   percents[1] = 0.05;
+   percents[2] = 0.15;
+   percents[3] = 0.15;
+   percents[4] = 0.20;
+   percents[5] = 0.40;
+  }
+}

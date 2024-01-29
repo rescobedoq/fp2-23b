daaf59b75915bbde0f9ee746f605ca6ce6964423
Richart Escobedo <rescobedoq@unsa.edu.pe>
2024-01-29 16:23:45 -0500

    fp2

---
+++ fase03/p3/file/WriteToFile.java
@@ -0,0 +1,18 @@
+package file;
+
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class WriteToFile {
+  public static void main(String[] args) {
+    try {
+      FileWriter myWriter = new FileWriter("filename.txt");
+      myWriter.write("I like programming.");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
